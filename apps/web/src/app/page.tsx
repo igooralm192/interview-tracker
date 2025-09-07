52ad255e0a6174d83005ce66a9792d6d033d91fe
Igor de Almeida <igoor1205@gmail.com>
2025-09-07 19:01:06 -0300

    feat(web): add web and web-e2e apps using Next.js, Tailwind and Shadcn/ui

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { Button } from "@/components/ui/button";
+
+export default function Index() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline font-sans text-primary">Welcome to web</h1>
+      <Button size='lg'>Click me</Button>
+    </div>
+  );
+}
